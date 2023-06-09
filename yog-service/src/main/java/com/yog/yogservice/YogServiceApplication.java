d67b6b5f8f7e12a574ff83a6f42a39e342496287
Wmw <1375194823@qq.com>
2023-06-09 11:10:28 +0800

    项目构建

---
+++ yog-service/src/main/java/com/yog/yogservice/YogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yog.yogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogServiceApplication.class, args);
+    }
+
+}
---
+++ yog-service/src/test/java/com/yog/yogservice/YogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yog.yogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
